1b3febd5daeb3024ea88d0b625c4c2e6d019f163
haiming <hesmaeilz01@gmail.com>
2021-10-16 17:39:40 -0400

    postgres

---
+++ boot_camp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = [process.argv[2] || 'JUL02'];
+const values = [`%${cohortName}%`]
+
+pool.query(`
+SELECT DISTINCT 
+  teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
